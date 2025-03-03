4c07fd4b87a08b96a4098a027706daaa53602707
Adam H <ad.hauck@gmail.com>
2025-03-03 14:36:16 -0800

    Add scroll-to-top script and custom image sizes in functions.php

--- functions.php
+++ functions.php
@@ -19,11 +19,27 @@ function mindset_enqueues() {
         '12.1.0'
     );
 
+    //load scroll-to-top.js on front-end
+    wp_enqueue_script(
+        'mindset-scroll-to-top',
+        get_template_directory_uri( ).'/assets/js/scroll-to-top.js',
+        array(),
+        wp_get_theme()->get('Version'),
+        array('strategy' => 'defer')
+    );
+
 }
 
 add_action('wp_enqueue_scripts', 'mindset_enqueues');
 
 function mindset_setup() {
+    //add style.css to editor
     add_editor_style( get_stylesheet_uri() );
+
+    //add custom image sizes
+    add_image_size( '400x500', 400, 500, true );
+    add_image_size( '200x250', 200, 250, true );
+    
 }
 add_action('after_setup_theme', 'mindset_setup');
+
