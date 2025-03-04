00ee7119c662105022aef604ffc719fa6326dd1f
Adam H <ad.hauck@gmail.com>
2025-03-04 15:07:05 -0800

    Add scroll-to-top script for contact page special color and enhance image sizes

---
+++ assets/js/contact-scroll-to-top.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    const button = document.getElementById('scroll-top');
+    
+    button.style.color = 'red';
+});
--- functions.php
+++ functions.php
@@ -28,6 +28,16 @@ function mindset_enqueues() {
         array('strategy' => 'defer')
     );
 
+    if(is_page(15)) {
+        wp_enqueue_script(
+            'mindset-contact-scroll-to-top',
+            get_template_directory_uri( ).'/assets/js/contact-scroll-to-top.js',
+            array('mindset-scroll-to-top'),
+            wp_get_theme()->get('Version'),
+            array('strategy' => 'defer')
+        );
+    }
+
 }
 
 add_action('wp_enqueue_scripts', 'mindset_enqueues');
@@ -39,6 +49,8 @@ function mindset_setup() {
     //add custom image sizes
     add_image_size( '400x500', 400, 500, true );
     add_image_size( '200x250', 200, 250, true );
+    add_image_size( '400x200', 400, 200, true );
+    add_image_size( '800x400', 800, 400, true );
     
 }
 add_action('after_setup_theme', 'mindset_setup');
@@ -48,6 +60,8 @@ function mindset_add_custom_image_sizes( $size_names ) {
 	$new_sizes = array(
 		'400x500' => __( '400x500', 'mindset-theme' ),
 		'200x250' => __( '200x250', 'mindset-theme' ),
+        '400x200' => __( '400x200', 'mindset-theme' ),
+        '800x400' => __( '800x400', 'mindset-theme' ),
 	);
 	return array_merge( $size_names, $new_sizes );
 }
